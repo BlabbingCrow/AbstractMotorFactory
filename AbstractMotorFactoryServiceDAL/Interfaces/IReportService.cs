4c9114c80f619b9f74311ba482e010792ee69c39
Danil <anikyev95@mail.ru>
2019-03-30 14:23:08 +0400

    Лаб 4 временный коммит

---
+++ AbstractMotorFactoryServiceDAL/Interfaces/IReportService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AbstractMotorFactoryServiceDAL.Interfaces
+{
+    public interface IReportService
+    {
+        void SaveProductPrice(ReportBindingModel model);
+
+        List<StorageLoadViewModel> GetStocksLoad();
+
+        void SaveStocksLoad(ReportBindingModel model);
+
+        List<CustomerProductionModel> GetClientOrders(ReportBindingModel model);
+
+        void SaveClientOrders(ReportBindingModel model);
+    }
+}
---
+++ AbstractMotorFactoryServiceDAL/ViewModels/CustomerProductionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace AbstractMotorFactoryServiceDAL.ViewModels
+{
+    public class CustomerProductionsViewModel
+    {
+        public string CustomerName { get; set; }
+
+        public string TimeCreate { get; set; }
+
+        public string EngineName { get; set; }
+
+        public int Number { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string State { get; set; }
+    }
+}
