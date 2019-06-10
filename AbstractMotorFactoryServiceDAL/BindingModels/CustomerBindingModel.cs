85bcc0553c6febf2bbbff107f2975d6423672c1f
Данил Аникьев <anikyev95@mail.ru>
2019-06-10 16:43:06 +0400

    Изменен интерфейс

--- AbstractMotorFactoryServiceDAL/BindingModels/CustomerBindingModel.cs
+++ AbstractMotorFactoryServiceDAL/BindingModels/CustomerBindingModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace AbstractMotorFactoryServiceDAL.BindingModels
     {
         public int Id { get; set; }
 
+        [DisplayName("ФИО покупателя")]
         public string CustomerFIO { get; set; }
     }
 }
